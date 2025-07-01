2e5c1c3e25f07e7ac506d4bf87b1018c49fe5bdc
André Felipe Becker Vargas <12925114987@ulife.com.br>
2025-07-01 10:13:47 -0300

    Versão inicial do projeto Olá, Mundo

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,8 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) {
+        // Este é o ponto de partida do nosso programa
+        System.out.println("Olá, Mundo! Meu ambiente Java está funcionando!");
+    }
+
+}
