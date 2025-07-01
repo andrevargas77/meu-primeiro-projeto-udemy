ae765c0f5e137996ae1faf97d50c879b7572469f
André Felipe Becker Vargas <12925114987@ulife.com.br>
2025-07-01 10:37:13 -0300

    Modificada a mensagem de saudação

--- MeuPrimeiroPrograma.java
+++ MeuPrimeiroPrograma.java
@@ -2,7 +2,7 @@ public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
         // Este é o ponto de partida do nosso programa
-        System.out.println("Olá, Mundo! Meu ambiente Java está funcionando!");
+        System.out.println("Olá, Mundo! Meu ambiente Java está funcionando! e é minha primeira vez aqui");
     }
 
 }
